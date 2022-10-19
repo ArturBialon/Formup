3a3e6d6fc738d5e79d9683c9bfd35d6421fc589a
s18995 <s18995@pjwstk.edu.pl>
2022-10-19 19:32:02 +0200

    ssl and bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Formup';
+  users: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/Forwarders')
+    .subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error)
+    })
+  }
+
 }
