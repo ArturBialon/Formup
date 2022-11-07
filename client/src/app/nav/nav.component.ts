fb72b85bc9a1cecdcd3ed345dd27a9a8245d4f8e
s18995 <s18995@pjwstk.edu.pl>
2022-11-07 21:26:03 +0100

    update 14

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 import { User } from '../models/user';
 import { AccountService } from '../_services/account.service';
 
