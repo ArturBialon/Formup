45d7b16f2705fc981d2eac221eeb2ce48f24ff51
s18995 <s18995@pjwstk.edu.pl>
2022-11-09 18:30:57 +0100

    errors

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,56 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/bug")]
+    public class BuggyController : ControllerBase
+    {
+        private readonly FWD_CompContext _context;
+        public BuggyController(FWD_CompContext compContext) 
+        {
+            _context = compContext;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret() 
+        {
+            return Ok("secret text");
+        }
+
+        //[Authorize]
+        [HttpGet("not-found")]
+        public ActionResult<Forwarder> GetNotFound()
+        {
+            var thing = _context.Forwarders.Find(-1);
+            if (thing == null) return NotFound();
+            return Ok();
+        }
+
+        //[Authorize]
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+
+                var thing = _context.Forwarders.Find(-1);
+                var thingToReturn = thing.ToString();
+
+                return thingToReturn;
+            
+        }
+
+        //[Authorize]
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("not gooood bro");
+        }
+    }
+}
