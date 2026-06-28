e3870d449e0d9e0d8cccd093033b1808c0ea1895
Artur Białoń <4.bialon@gmail.com>
2026-06-28 22:51:15 +0200

    won 21

--- client/src/app/errors/server-error/server-error.component.ts
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -2,20 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-server-error',
-    templateUrl: './server-error.component.html',
-    styleUrls: ['./server-error.component.css'],
-    standalone: false
+  selector: 'app-server-error',
+  templateUrl: './server-error.component.html',
+  styleUrls: ['./server-error.component.css'],
+  standalone: false,
 })
 export class ServerErrorComponent implements OnInit {
   error: any;
 
-  constructor(private router: Router) { 
-    const navigation = this.router.getCurrentNavigation();
+  constructor(private router: Router) {
+    const navigation = this.router.currentNavigation();
     this.error = navigation?.extras?.state?.error;
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
